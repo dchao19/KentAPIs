0f4ea51e15e74b89cffc92663482dcb3fd193a13
Daniel Chao <dchao19@kentdenver.org>
2015-11-09 10:36:47 -0700

    Push up to clement github cause cool.

---
+++ index.js
@@ -0,0 +1,84 @@
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+var moment = require('moment');
+var ical = require('ical'), months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+function getTodaySchedule(){
+	var returnDay = {};
+	ical.fromURL('http://www.kentdenver.org/calendar/calendar_351.ics', {}, function(err, data) {
+      for (var k in data){
+        if (data.hasOwnProperty(k)) {
+          var ev = data[k];
+          var dateTime = moment(ev.start);
+          
+
+          if(dateTime.isSame(new Date(), 'day')){
+          	var returnDay1 = {
+          		scheduleDay: ev.summary.substring(0,1),
+          		dateTime: moment(ev.start)
+          	}	
+          	console.log(returnDay1);
+          	return returnDay1;
+          }
+          
+        }
+      }
+    });	
+}
+
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+today = moment();
+todayScheduleDay = getTodaySchedule;
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    var returnDay1 = {};
+    ical.fromURL('http://www.kentdenver.org/calendar/calendar_351.ics', {}, function(err, data) {
+      for (var k in data){
+        var ev = data[k];
+        var dateTime = moment(ev.start);  
+        if(dateTime.isSame(new Date(), 'day')){
+        	var scheduleDayLetter = ev.summary.substring(0,1)
+          	switch(scheduleDayLetter){
+          		case "F":
+          			var timeRightNow = moment();
+          			if(timeRightNow.isAfter)
+          			break;
+          	}
+
+          	returnDay1 = {
+          		scheduleDay: scheduleDayLetter,
+          		dateTime: moment(ev.start)
+          	}
+          	res.json(returnDay1); 	
+        }
+      }
+    });	    
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
+
