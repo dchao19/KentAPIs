06f617b34ce28fa7c6a7caafa962d212eb62fd11
Daniel Chao <dchao19@kentdenver.org>
2015-11-09 10:42:01 -0700

    Slight modification to API response protocol  - check API status at root and get schedule day at api/getScheduleDay

--- index.js
+++ index.js
@@ -15,38 +15,17 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
 
-function getTodaySchedule(){
-	var returnDay = {};
-	ical.fromURL('http://www.kentdenver.org/calendar/calendar_351.ics', {}, function(err, data) {
-      for (var k in data){
-        if (data.hasOwnProperty(k)) {
-          var ev = data[k];
-          var dateTime = moment(ev.start);
-          
-
-          if(dateTime.isSame(new Date(), 'day')){
-          	var returnDay1 = {
-          		scheduleDay: ev.summary.substring(0,1),
-          		dateTime: moment(ev.start)
-          	}	
-          	console.log(returnDay1);
-          	return returnDay1;
-          }
-          
-        }
-      }
-    });	
-}
-
-
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 today = moment();
-todayScheduleDay = getTodaySchedule;
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
+router.get('/', function(req,res){
+	res.json({message: "API OK"});
+});
+
+router.get('/getScheduleDay', function(req, res) {
     var returnDay1 = {};
     ical.fromURL('http://www.kentdenver.org/calendar/calendar_351.ics', {}, function(err, data) {
       for (var k in data){
@@ -54,13 +33,6 @@ router.get('/', function(req, res) {
         var dateTime = moment(ev.start);  
         if(dateTime.isSame(new Date(), 'day')){
         	var scheduleDayLetter = ev.summary.substring(0,1)
-          	switch(scheduleDayLetter){
-          		case "F":
-          			var timeRightNow = moment();
-          			if(timeRightNow.isAfter)
-          			break;
-          	}
-
           	returnDay1 = {
           		scheduleDay: scheduleDayLetter,
           		dateTime: moment(ev.start)
