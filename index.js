a13459775ab12706c8d0e36c4530deae7d038224
Daniel Chao <dchao19@kentdenver.org>
2015-11-09 20:13:28 -0700

    The worst callback system ever - ever. Also added another api under localhost:8080/apiv2 for testing purposes. Added current period and right now is set to testing mode - the "today" moment is 11/9/2015 at 1:25 pm.
    Next - next period (ha!).

--- index.js
+++ index.js
@@ -18,37 +18,76 @@ var port = process.env.PORT || 8080;        // set our port
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
-today = moment();
-
+today = moment("2015-11-9 1:25 PM","YYYY-MM-DD hh:mm A"); //global instance of right now in moment
+var router2 = express.Router();
+scheduleDayLetter = ""; // I'm polluting the global scope but I don't even care!
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req,res){
 	res.json({message: "API OK"});
 });
 
+router2.get('/', function(req,res){
+	res.json({message: "API v2 OK"});
+});
+
 router.get('/getScheduleDay', function(req, res) {
-    var returnDay1 = {};
-    ical.fromURL('http://www.kentdenver.org/calendar/calendar_351.ics', {}, function(err, data) {
+    var day;
+    var period;
+    
+    getScheduleDay(function(result){
+    	day = result;
+    	getCurrentPeriod(function(result){
+    		period = result;
+    		respond(day, period);
+    	});   
+    });	
+
+    function respond(scheduleDay, period){
+    	res.json({scheduleDay: scheduleDay, nowPeriod: period})
+    };    
+});
+
+function getScheduleDay(callback){
+
+	ical.fromURL('http://www.kentdenver.org/calendar/calendar_351.ics', {}, function(err, data) {
       for (var k in data){
         var ev = data[k];
         var dateTime = moment(ev.start);  
-        if(dateTime.isSame(new Date(), 'day')){
-        	var scheduleDayLetter = ev.summary.substring(0,1)
-          	returnDay1 = {
-          		scheduleDay: scheduleDayLetter,
-          		dateTime: moment(ev.start)
-          	}
-          	res.json(returnDay1); 	
+        if(dateTime.isSame(today, 'day')){
+        	callback(ev.summary.substring(0,1).trim());
         }
       }
-    });	    
-});
+    });
+}
 
+function getCurrentPeriod(callback){
+	var current = {};
+
+	ical.fromURL('http://files.danielchao.me/kent/calendarWithPeriods.ics', {}, function(err, data) {
+    	for (var k in data){
+        	var ev = data[k];        
+        	var startDateTime = moment(ev.start); 
+        	var endDateTime = moment(ev.end);
+        
+        	if(today.isBetween(startDateTime,endDateTime,'minute')) {
+        		current = {
+        			period: ev.summary,
+        			startTime: startDateTime.format("hh:mm a"),
+        			endTime: endDateTime.format("hh:mm a")
+        		}
+        		callback(current);
+ 			}     
+		}
+	});	
+}
+
+	
 // more routes for our API will happen here
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
-
+app.use('/apiv2', router2);
 // START THE SERVER
 // =============================================================================
 app.listen(port);
