e8eb078b81d0fb748a0f9aa42d0ff8274e9876ff
Daniel Chao <daniel@danielchao.me>
2016-05-01 18:15:50 -0600

    Simplify auth middleware

--- routes/schedule.js
+++ routes/schedule.js
@@ -211,24 +211,7 @@ router.post('/get-token', function(req, res) {
 
 //Auth
 
-router.use(function(req, res, next) {
-        var token = req.body.token || req.query.token || req.headers['x-access-token']
-        if(token) {
-                jwt.verify(token, secret, function(err, decoded) {
-                        if(err) {
-                                res.json({success:false, message:'Invalid token'});
-                        } else {
-                                req.decoded = decoded;
-                                next();
-                        }
-                });
-        } else {
-                res.status(403).send({
-                        success: false,
-                        message: 'No token provided'
-                });
-        }
-});
+router.use(utils.strictAuthentication);
 
 /********************************
  * Authenticated Routes
