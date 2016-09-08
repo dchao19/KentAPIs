0f453cc0186a26b8596fc653c3d3d45703c31ca2
Daniel Chao <daniel@danielchao.me>
2016-09-08 11:53:22 -0600

    Add port to config file

--- config.js
+++ config.js
@@ -5,5 +5,6 @@ var databaseUrl = process.env.NODE_ENV === 'testing' ?
 module.exports = {
     //currently using 64 bits for testing, likely want to go to at least 256, also need to conceal in production
     'secret': '<FILL_IN_YOUR_SECRET>',
-    'database': databaseUrl
+    'database': databaseUrl,
+    port: 8080
 }
