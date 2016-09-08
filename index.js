bd13413d2e3bce0ee35f4319ddd241f0afa7e394
Daniel Chao <daniel@danielchao.me>
2016-09-08 11:56:09 -0600

    ACTUALLY add the port to the config

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var app = require('./app.js');
+var config = require('./config.js');
 // START THE SERVER
 // =============================================================================
-app.listen(port);
+app.listen(config.port);
 console.log('Magic happens on port ' + port);
